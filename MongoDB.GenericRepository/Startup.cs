f3045813d0472f054b4992714d9cb79581058a55
Chansak <chansak_noi@hotmail.com>
2024-03-15 14:04:40 +0700

    init

---
+++ MongoDB.GenericRepository/Startup.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using MongoDB.GenericRepository.Context;
+using MongoDB.GenericRepository.Interfaces;
+using MongoDB.GenericRepository.Persistence;
+using MongoDB.GenericRepository.Repository;
+using MongoDB.GenericRepository.UoW;
+using System;
+
+namespace MongoDB.GenericRepository
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0)
+                .AddJsonOptions(opt =>
+                {
+                    var serializerOptions = opt.JsonSerializerOptions;
+                    serializerOptions.IgnoreNullValues = true;
+                    serializerOptions.IgnoreReadOnlyProperties = false;
+                    serializerOptions.WriteIndented = true;
+                });
+
+            // Configure the persistence in another layer
+            MongoDbPersistence.Configure();
+
+            services.AddSwaggerGen(s =>
+            {
+                s.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "MongoDB Repository Pattern and Unit of Work - Example",
+                    Description = "Swagger surface",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Bruno Brito",
+                        Email = "bhdebrito@gmail.com",
+                        Url = new Uri("http://www.brunobrito.net.br")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "MIT",
+                        Url = new Uri("https://github.com/brunohbrito/MongoDB-RepositoryUoWPatterns/blob/master/LICENSE")
+                    }
+                });
+            });
+
+            RegisterServices(services);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.UseSwagger(c =>
+            {
+                c.RouteTemplate = "swagger/{documentName}/swagger.json";
+            });
+
+            app.UseSwaggerUI(s =>
+            {
+                s.SwaggerEndpoint("/swagger/v1/swagger.json", "Repository Pattern and Unit of Work API v1.0");
+            });
+        }
+
+        private void RegisterServices(IServiceCollection services)
+        {
+            services.AddScoped<IMongoContext, MongoContext>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+        }
+    }
+}
